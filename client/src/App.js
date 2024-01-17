626e4e2b0300f93a8189dce315832f6891bb9ffa
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-01-17 12:53:34 +0200

    initial commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
 
-  // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://https://c-xkx8.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
-  }, []);
+  },[]);
 
   return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
+  <div className="App">
+    <h1>{message}</h1>
+  </div>
   );
 }
 
